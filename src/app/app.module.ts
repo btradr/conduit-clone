439f40743ff47da7f7acefecb76c6780c227fb21
Andrew Baturin <baturin.andrew1501@gmail.com>
2022-10-22 19:39:15 +0300

    refactor: added categories for imports and imported the authorization module in root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modules
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
